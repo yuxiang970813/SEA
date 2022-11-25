c32df095cc3e3df487aa2c204a1c57fb6971a881
yuxiang970813 <yuxiang19970813@hotmail.com>
2022-11-25 13:20:28 +0800

    Create and setup coursework app

--- SEA/settings.py
+++ SEA/settings.py
@@ -21,17 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = "coursework.User"
 
 # Application definition
 
 INSTALLED_APPS = [
+    'coursework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
