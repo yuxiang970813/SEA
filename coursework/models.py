290ef3c398503d2c2ce0b1e54dac0d422782839c
yuxiang970813 <yuxiang19970813@hotmail.com>
2022-12-03 12:53:57 +0800

    Declare expired function in assignment model

--- coursework/models.py
+++ coursework/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from datetime import datetime
 import os
 
 
@@ -68,6 +69,10 @@ class Assignment(models.Model):
 
     def __str__(self):
         return self.title
+
+    @property
+    def is_expired(self):
+        return datetime.now() > self.deadline
 
     class Meta:
         ordering = ["coursework", "-created_on"]
